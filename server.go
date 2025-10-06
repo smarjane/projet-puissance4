4cab47392250a01c34f6ccc73df8464038652e01
smarjane <marjane.selman@ynov.com>
2025-10-06 17:53:58 +0200

    debut

---
+++ server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type Student struct {
+	Name  string
+	Age   string
+	Quote string
+	Hobby string
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	template, err := template.ParseFiles("./index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	template.Execute(w, nil)
+}
+func House(w http.ResponseWriter, r *http.Request) {
+	template, err := template.ParseFiles("./index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	template.Execute(w, nil)
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/idcard", House)
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.ListenAndServe(":8080", nil)
+}
